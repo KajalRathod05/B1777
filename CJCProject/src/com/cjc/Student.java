f194368f65c26e606f96e7c37350794491f2d6e4
Kajal Rathod <120319504+kajalmanoharrathod@users.noreply.github.com>
2024-07-09 18:56:47 +0530

    Update Student.java
    add m2 method

--- CJCProject/src/com/cjc/Student.java
+++ CJCProject/src/com/cjc/Student.java
@@ -6,6 +6,12 @@ public class Student {
 	{
           System.out.println("Student---m1");
 	}
+
+	public void m2()
+	{
+          System.out.println("Student---m2");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello..");
