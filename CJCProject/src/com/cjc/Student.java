41fec16c041782ac4c74423cb9246c7929bb797e
Kajal Rathod <120319504+kajalmanoharrathod@users.noreply.github.com>
2024-07-09 18:45:47 +0530

    Update Student.java

--- CJCProject/src/com/cjc/Student.java
+++ CJCProject/src/com/cjc/Student.java
@@ -2,6 +2,10 @@ package com.cjc;
 
 public class Student {
 
+	public void m1()
+	{
+          System.out.println("Student---m1");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Hello..");
